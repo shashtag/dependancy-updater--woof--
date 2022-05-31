96ea1ca1dc241a5b1386c9f84cdec490288c4f52
shahtag <shashwatsatna@gmail.com>
2022-05-31 16:06:21 +0530

    ✨ parse csv file

--- dependancy-updater/index.js
+++ dependancy-updater/index.js
@@ -10,6 +10,10 @@
 const init = require('./utils/init');
 const cli = require('./utils/cli');
 const log = require('./utils/log');
+const alert = require('cli-alerts');
+let path = require('path');
+let fs = require('fs');
+let { parse } = require('csv-parse');
 
 const input = cli.input;
 const flags = cli.flags;
@@ -19,5 +23,36 @@ const { clear, debug } = flags;
 	init({ clear });
 	input.includes(`help`) && cli.showHelp(0);
 
+	if (flags.csv && flags.package) {
+		if (flags.csv.substr(flags.csv.length - 4) !== '.csv') {
+			alert({
+				type: `error`,
+				name: `Args Error`,
+				msg: `Enter a file with .csv extension`
+			});
+		}
+		const csvpath = path.join(process.cwd(), flags.csv);
+		const csvData = [];
+		fs.createReadStream(csvpath)
+			.pipe(
+				parse({
+					columns: true,
+					delimiter: [';', ',', ' ', '\t', '|'],
+					bom: true
+				})
+			)
+			.on('data', dataRow => {
+				csvData.push(dataRow);
+			})
+			.on('end', () => console.table(csvData));
+	}
+
+	if ((flags.csv && !flags.package) || (!flags.csv && flags.package)) {
+		alert({
+			type: `error`,
+			name: `Args Error`,
+			msg: `Add both csv file using flag '-c' and package using flag '-p'`
+		});
+	}
 	debug && log(flags);
 })();
